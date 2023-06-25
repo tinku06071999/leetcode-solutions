708cda7dee48749228ada475eb1efe26a87f7232
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-06-25 11:22:04 +0530

    Time: 6 ms (12.28%), Space: 7.9 MB (18.04%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
+// recursion without memoization
+  // int solve(int idx,vector<int>&nums,vector<int>&dp){
+  //     if(idx==0) return nums[0];
+  //     if(idx<0) return 0;
+  //     int pick = nums[idx]+solve(idx-2,nums,dp);
+  //     int notPick = 0+solve(idx-1,nums,dp);
+  //     return= max(pick,notPick);
+   
+  // }
+  // with memoization
   int solve(int idx,vector<int>&nums,vector<int>&dp){
       if(idx==0) return nums[0];
-      if(idx<0)return 0;
+      if(idx<0) return 0;
       if(dp[idx]!= -1)return dp[idx];
       int pick = nums[idx]+solve(idx-2,nums,dp);
       int notPick = 0+solve(idx-1,nums,dp);
       dp[idx]= max(pick,notPick);
       return dp[idx];
   }
-  // int solve(int idx,vector<int>&nums,int dp[]){
-  //   int ans = 0;
-  //   for(int i=idx;i>=0;i++){
-  //     if(idx==0){
-
-  //     }
-  //     if(idx<0)return 0;
-  //     int pick = nums[i]+nums[i-2];
-  //     int notPick = 0+nums[i-1];
-  //     ans = max(pick,notPick);
-  //   }
-  //   return ans;
-  // }
     int rob(vector<int>& nums) {
         int n =nums.size();
     //     int i=0;
