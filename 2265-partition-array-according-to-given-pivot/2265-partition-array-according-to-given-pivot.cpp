78ad7087bfcb6f8bd5e7512874352e9ebb3ab1f8
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-08-09 12:59:12 +0530

    Time: 151 ms (94.5%), Space: 129.2 MB (50.78%) - LeetHub

--- 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/2265-partition-array-according-to-given-pivot.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        // vector<int>ans(nums.size());
-        vector<int>lesser;
-        vector<int>greater;
-        vector<int>equal;
-        for(int i=0;i<nums.size();i++ ){
-            if(nums[i]<pivot)lesser.push_back(nums[i]);
-            else if(nums[i]==pivot)equal.push_back(nums[i]);
-            else greater.push_back(nums[i]);
-        }
-        for(int i=0;i<lesser.size();i++){
-            nums[i]=lesser[i];
-        } int idx = lesser.size();
-        for(int i=0;i<equal.size();i++){
-            nums[idx++]=equal[i];
-        }idx = lesser.size()+equal.size(); 
-        for(int i=0;i<greater.size();i++){
-            nums[idx++]=greater[i];
-        }
-       return nums;
+        vector<int>ans;
+    //     vector<int>lesser;
+    //     vector<int>greater;
+    //     vector<int>equal;
+    //     for(int i=0;i<nums.size();i++ ){
+    //         if(nums[i]<pivot)lesser.push_back(nums[i]);
+    //         else if(nums[i]==pivot)equal.push_back(nums[i]);
+    //         else greater.push_back(nums[i]);
+    //     }
+    //     for(int i=0;i<lesser.size();i++){
+    //         nums[i]=lesser[i];
+    //     } int idx = lesser.size();
+    //     for(int i=0;i<equal.size();i++){
+    //         nums[idx++]=equal[i];
+    //     }idx = lesser.size()+equal.size(); 
+    //     for(int i=0;i<greater.size();i++){
+    //         nums[idx++]=greater[i];
+    //     }
+    //    return nums;
+    
+         for(int i=0;i<nums.size();i++){
+             if(nums[i]<pivot)
+                 ans.push_back(nums[i]);
+         }for(int i=0;i<nums.size();i++){
+             if(nums[i]==pivot)
+                 ans.push_back(nums[i]);
+         }for(int i=0;i<nums.size();i++){
+             if(nums[i]>pivot)
+                 ans.push_back(nums[i]);
+         }
+         return ans;
     }
 };
