bbde49ee459ab72326cfdf870908de96c3f14bb3
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-08-19 17:17:01 +0530

    Time: 0 ms (100%), Space: 14.1 MB (23.33%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,9 +1,34 @@
 class Solution {
+private: 
+    int pivot(vector<int>& nums){
+         int n = nums.size();
+         int left=0;
+         int right=n-1;
+         int mid = (left+right)/2;
+         while(left<=right){
+             if(mid>0 && nums[mid]<nums[mid-1]){
+                return mid;
+            }
+             
+             else if(nums[left]==nums[right]){ //when duplicate is present 
+                left++;
+            }
+            else if(nums[mid]>=nums[left] && nums[mid]>nums[right]){
+                left=mid+1;
+
+            }
+             else right = mid-1;
+             mid = (left+right)/2;
+         }
+         return left;
+    }
 public:
     bool search(vector<int>& nums, int target) {
         int n = nums.size();
         if(nums[0]==target)return true;
         if(nums[n-1]==target)return true;
+        // int pivot_idx = pivot(nums);
+        // cout<<pivot_idx;
         int s=0;
         int e = n-1;
         int mid = (s+e)/2;
