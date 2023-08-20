6a6e89b9978423e97bc7b43e8eac8d710b87d81d
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-08-20 19:31:54 +0530

    Time: 6 ms (27.96%), Space: 9.4 MB (95.8%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -5,15 +5,17 @@ public:
 			cols = matrix[0].size(),
             r = 0, c = cols - 1;
             int mid = (r+c)/2;
-		while(r <rows && c >=0){
-            // mid = (row+col)/2;
-            int s = 0;
-            int e = c;
-            while(s<=e){
-              mid = (s+e)/2;
-              if(matrix[r][mid]==target)return true;
-              else if(matrix[r][mid]>target) e=mid-1;
-              else s = mid+1;
+		while(r < rows){
+            
+            if(matrix[r][0]<=target && matrix[r][c]>=target){
+                int s = 0;
+                int e = c;
+                    while(s<=e){
+                        mid = (s+e)/2;
+                        if(matrix[r][mid]==target)return true;
+                        else if(matrix[r][mid]>target) e=mid-1;
+                        else s = mid+1;
+                    }
             }
             r++;
         }
