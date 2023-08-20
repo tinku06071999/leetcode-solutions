b5554dc156e33a1079a7cd30f682047a2e45b2c1
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-08-20 20:02:47 +0530

    Time: 3 ms (75.39%), Space: 9.5 MB (67.09%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,24 +1,44 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int rows = matrix.size(),
-			cols = matrix[0].size(),
-            r = 0, c = cols - 1;
-            int mid = (r+c)/2;
-		while(r < rows){
+        // (m*log(n))
+        // int rows = matrix.size(),
+		// 	cols = matrix[0].size(),
+        //     r = 0, c = cols - 1;
+        //     int mid = (r+c)/2;
+		// while(r < rows){
             
-            if(matrix[r][0]<=target && matrix[r][c]>=target){
-                int s = 0;
-                int e = c;
-                    while(s<=e){
-                        mid = (s+e)/2;
-                        if(matrix[r][mid]==target)return true;
-                        else if(matrix[r][mid]>target) e=mid-1;
-                        else s = mid+1;
-                    }
-            }
-            r++;
-        }
-        return false;
+        //     if(matrix[r][0]<=target && matrix[r][c]>=target){
+        //         int s = 0;
+        //         int e = c;
+        //             while(s<=e){
+        //                 mid = (s+e)/2;
+        //                 if(matrix[r][mid]==target)return true;
+        //                 else if(matrix[r][mid]>target) e=mid-1;
+        //                 else s = mid+1;
+        //             }
+        //     }
+        //     r++;
+        // }
+        // return false;
+      
+
+      //second approach O(log(m*n));
+      
+      int m = matrix.size();
+	  int n = matrix[0].size();
+      int s=0;
+      int e = m*n-1;
+      int mid = (s+e)/2;
+      while(s<=e){
+          mid = (s+e)/2;
+          int row = mid / n ;
+          int col = mid % n;
+          if(matrix[row][col]==target)return true;
+          else if(matrix[row][col] > target) e = mid-1;
+          else s = mid+1;
+          mid = (s+e)/2;
+      }
+      return false;
     }
 };
