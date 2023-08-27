832c9c8e48e151f300c2ea25e3dfcc2c6c81089b
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-08-27 22:23:34 +0530

    Time: 97 ms (54.81%), Space: 45.9 MB (20.23%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -9,8 +9,23 @@ public:
         return dp[idx][amount] = min(pick,not_pick);   
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>>dp(coins.size()+1,vector<int>(amount+1,-1));
+        vector<vector<int>>dp(coins.size(),vector<int>(amount+1,-1));
         if(solve(coins,amount,coins.size()-1,dp)>=1e8)return -1;
         return solve(coins,amount,coins.size()-1,dp);
+        int n = coins.size();
+        for(int i=0;i<n+1;i++){
+            for(int j=0;j<amount+1;j++){
+                 if (i == 0 || j == 0)
+                    dp[i][j] = (j == 0) ? 0 : INT_MAX - 1;
+            }
+        }
+        for(int i=0;i<n+1;i++){
+            for(int j=1;j<amount+1;j++){
+                int pick = 1+dp[i][j-1];
+                int not_pick = dp[i-1][j];
+                dp[i][j] = min(pick,not_pick);
+            }
+        }
+        return dp[n-1][amount-1];
     }
 };
