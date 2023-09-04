ea7239b599b521132a397704314a4e929c7f8b75
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-09-04 11:03:28 +0530

    Time: 20 ms (89.44%), Space: 23.4 MB (20.56%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,12 +11,21 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(!root || root==p || root==q)return root;
-        if(p->val< root->val && q->val > root->val)return root;
-        TreeNode* left = lowestCommonAncestor(root->left,p,q);
-        TreeNode* right = lowestCommonAncestor(root->right,p,q);
-        if(!left)return right;
-        if(!right)return left;
-        else return root;
+        // if(!root || root==p || root==q)return root;
+        // if(p->val< root->val && q->val > root->val)return root;
+        // TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        // TreeNode* right = lowestCommonAncestor(root->right,p,q);
+        // if(!left)return right;
+        // if(!right)return left;
+        // else return root;
+       
+        if(!root)return root;
+        int curr = root->val;
+        // if((curr > p->val && curr < q->val ) || (curr > q->val && curr < p->val ))       return root;
+        if(curr< p->val && curr < q->val)
+        return lowestCommonAncestor(root->right,p,q);
+        if(curr > p->val && curr > q->val)
+        return lowestCommonAncestor(root->left, p, q);
+        return root; 
     }
 };
