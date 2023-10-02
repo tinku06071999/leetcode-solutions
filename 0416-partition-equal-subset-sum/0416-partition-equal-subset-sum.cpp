92ea52aafd2885db5c177585f7699a421932c364
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-10-02 11:31:05 +0530

    Time: 237 ms (52.52%), Space: 95.3 MB (35.42%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums,int sum, int idx,vector<vector<int>> &dp){
-       if(sum==0)return true;
-       if(idx ==0 )return nums[idx] == sum;
-       if(dp[idx][sum] != -1)return dp[idx][sum];
-       bool not_take = solve(nums,sum,idx-1,dp);
-       bool take = false;
-       if(nums[idx] <= sum )
-       take = solve(nums,sum-nums[idx],idx-1,dp );
-       return (dp[idx][sum]  = (take || not_take));
+    bool solve(vector<int>&nums, int sum, int idx,vector<vector<int>>&dp){
+        if(sum==0)return true;
+        // if(idx<0)return false;
+        if(idx==0)return nums[idx]==sum;
+        if(dp[idx][sum] != -1)return dp[idx][sum];
+        bool not_pick = solve(nums,sum,idx-1,dp);
+        bool pick = false;
+        if(nums[idx]<=sum) pick = solve(nums,sum-nums[idx],idx-1,dp);
+        
+        return (dp[idx][sum] = (pick||not_pick));
     }
     bool canPartition(vector<int>& nums) {
-        int idx= nums.size()-1;
-        int sum =0;
-        for(int i=0;i<idx+1;i++)sum+=nums[i];
-        if(sum%2 !=0 )return false;
-        sum = sum / 2;
-        vector<vector<int>>dp(idx+1, vector<int>(sum+1,-1));
-        return solve(nums,sum,idx,dp);
+        int sum=0;
+        int n = nums.size();
+        
+        for(int i=0;i<n;i++)sum += nums[i];
+        if(sum%2 != 0)return false;
+        sum /=2;
+        vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        return solve(nums,sum,n-1,dp);
     }
 };
