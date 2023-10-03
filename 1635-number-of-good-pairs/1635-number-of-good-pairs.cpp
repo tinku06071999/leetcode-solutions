21823dc21dc74cba18362f395ec850a3ca5ace3a
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-10-03 10:48:58 +0530

    Time: 0 ms (100%), Space: 7.6 MB (16.85%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -3,11 +3,21 @@ public:
     int numIdenticalPairs(vector<int>& nums) {
         int count=0;
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i]==nums[j])count++;
-            }
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i]==nums[j])count++;
+        //     }
+        // }
+        // return count;
+
+        // second method
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++)mp[nums[i]]++;
+        for(auto i : mp){
+            if(i.second==0 || i.second == 1)continue;
+            int n = (i.second)-1;
+            count += (n*(n+1))/2;
         }
-        return count;
+       return count;
     }
 };
