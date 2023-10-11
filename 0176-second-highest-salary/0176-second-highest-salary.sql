7ebaf72eebbcda87c2b0f569e775edd768dae7e3
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-10-11 16:05:36 +0530

    Time: 1066 ms (5.01%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -8,20 +8,22 @@
 # select distinct salary as SecondHighestSalary from employee
 # order by salary desc
 # limit 1,1;
-SELECT MAX(salary) AS SecondHighestSalary 
-FROM employee e1
-WHERE 1 = (
-    # select COUNT (DISTINCT salary)
-    SELECT COUNT(DISTINCT salary)
-    FROM employee e2
-    WHERE e2.salary > e1.salary
-);
 
 
-# SELECT MAX(salary) AS SecondHighestSalary
+# SELECT MAX(salary) AS SecondHighestSalary 
 # FROM employee e1
 # WHERE 1 = (
+#     # select COUNT (DISTINCT salary)
 #     SELECT COUNT(DISTINCT salary)
 #     FROM employee e2
 #     WHERE e2.salary > e1.salary
 # );
+
+
+SELECT MAX(salary) AS SecondHighestSalary
+FROM employee e1
+WHERE 1 = (
+    SELECT COUNT(DISTINCT salary)
+    FROM employee e2
+    WHERE e2.salary > e1.salary
+);
