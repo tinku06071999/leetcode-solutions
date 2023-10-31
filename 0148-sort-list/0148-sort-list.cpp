b1faa270914845b9e79cf088a1d3b7a66b9d9fd1
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2023-10-31 17:57:50 +0530

    Time: 158 ms (36.06%), Space: 74.4 MB (23.94%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,21 +10,63 @@
  */
 class Solution {
 public:
+   ListNode* mergelist(ListNode* l1, ListNode* l2){
+       ListNode* ptr = new ListNode(0);
+       ListNode* curr = ptr;
+
+       while(l1 && l2){
+           if(l1->val <= l2->val){
+               curr->next = l1;
+               l1 = l1->next;
+           }
+           else{
+               curr->next = l2;
+               l2 = l2->next;
+           }
+           curr = curr->next;
+       }
+       if(l1){
+           curr->next = l1;
+           l1 = l1->next;
+       }
+       if(l2){
+           curr->next = l2;
+           l2 = l2->next;
+       }
+       return ptr->next;
+   }
     ListNode* sortList(ListNode* head) {
-        vector<int>v;
-        ListNode* temp = head;
-        while(temp){
-            v.push_back(temp->val);
-            temp = temp->next;
-        }sort(v.begin(), v.end());
-        ListNode* dummy = new ListNode();
-        ListNode* curr = new ListNode();
-        dummy->next = curr;
-        for(int i=0;i<v.size();i++){
-            ListNode* tmp = new ListNode(v[i]);
-            curr->next = tmp;
-            curr = tmp;
+        // vector<int>v;
+        // ListNode* temp = head;
+        // while(temp){
+        //     v.push_back(temp->val);
+        //     temp = temp->next;
+        // }sort(v.begin(), v.end());
+        // ListNode* dummy = new ListNode();
+        // ListNode* curr = new ListNode();
+        // dummy->next = curr;
+        // for(int i=0;i<v.size();i++){
+        //     ListNode* tmp = new ListNode(v[i]);
+        //     curr->next = tmp;
+        //     curr = tmp;
+        // }
+        // return dummy->next->next;
+
+        if(head ==nullptr || head->next ==nullptr)return head;
+
+        ListNode* temp = nullptr;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast  && fast->next){
+            temp = slow;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        return dummy->next->next;
+        temp->next = nullptr;
+        ListNode* l1 = sortList(head);
+        ListNode* l2 = sortList(slow);
+        return mergelist(l1,l2);
+
     }
 };
