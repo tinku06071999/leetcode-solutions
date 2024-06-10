72c08407595fd587d379ef3d1a8afefaa433f6dd
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2024-06-10 13:47:37 +0530

    Time: 0 ms (100%), Space: 9.8 MB (33.92%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -7,24 +7,45 @@ public:
 
         // method - 02
 
-        int cnt0 =0;
-        int cnt1 =0;
-        int cnt2 =0;
+        // int cnt0 =0;
+        // int cnt1 =0;
+        // int cnt2 =0;
 
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] == 0)cnt0++;
-            else if(nums[i] == 1)cnt1++;
-            else cnt2++;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i] == 0)cnt0++;
+        //     else if(nums[i] == 1)cnt1++;
+        //     else cnt2++;
+        // }
+        // for(int i= 0; i<cnt0; i++){
+        //     nums[i] =0;
+        // }
+        // for(int i = cnt0; i<cnt0+cnt1; i++){
+        //     nums[i] = 1;
+        // }
+        // for(int i = cnt0+cnt1; i<nums.size(); i++){
+        //     nums[i] = 2;
+        // }
+    
+
+       // method - 3;
+
+       int low = 0;
+       int high = nums.size()-1;
+       int mid = 0;
+
+       while( mid <= high){
+        if(nums[mid] == 0){
+            swap(nums[low], nums[mid]);
+            low++;
+            mid++;
         }
-        for(int i= 0; i<cnt0; i++){
-            nums[i] =0;
+        else if(nums[mid] == 2){
+            swap(nums[high], nums[mid]);
+            high--;
+
         }
-        for(int i = cnt0; i<cnt0+cnt1; i++){
-            nums[i] = 1;
-        }
-        for(int i = cnt0+cnt1; i<nums.size(); i++){
-            nums[i] = 2;
-        }
+        else mid++;
+       }
 
     }
 };
