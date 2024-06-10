bb44b7ac2825cdc455d1a32f4266795ef4573de4
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2024-06-10 13:37:21 +0530

    Time: 2 ms (44.07%), Space: 9.8 MB (71.61%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,6 +3,28 @@ public:
     void sortColors(vector<int>& nums) {
         
         // method - 01
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
+
+        // method - 02
+
+        int cnt0 =0;
+        int cnt1 =0;
+        int cnt2 =0;
+
+        for(int i=0;i<nums.size();i++){
+            if(nums[i] == 0)cnt0++;
+            else if(nums[i] == 1)cnt1++;
+            else cnt2++;
+        }
+        for(int i= 0; i<cnt0; i++){
+            nums[i] =0;
+        }
+        for(int i = cnt0; i<cnt0+cnt1; i++){
+            nums[i] = 1;
+        }
+        for(int i = cnt0+cnt1; i<nums.size(); i++){
+            nums[i] = 2;
+        }
+
     }
 };
