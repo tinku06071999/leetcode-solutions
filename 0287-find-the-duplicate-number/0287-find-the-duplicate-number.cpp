2cf61593ea652274518c07b73069bebc2127fc58
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2024-06-12 13:58:34 +0530

    Time: 68 ms (89.56%), Space: 63.5 MB (93.8%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // sort(nums.begin(), nums.end());
 
-        for(int i=1;i<nums.size();i++){
-            if(nums[i] == nums[i-1]){
-                return nums[i];
-            }
+        // for(int i=1;i<nums.size();i++){
+        //     if(nums[i] == nums[i-1]){
+        //         return nums[i];
+        //     }
+        // }
+        // return 0;
+       
+       for(auto i : nums){
+        i = abs(i);
+        if(nums[i] > 0){
+            nums[i] = -nums[i];
+        }else{
+            return i;
         }
-        return 0;
+       }
+       return nums.size();
+
     }
 };
