85b354bd1db8fe309b78da145922bc9432b51fdd
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2024-06-13 12:01:48 +0530

    Time: 7 ms (21.85%), Space: 12 MB (48.62%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        // (m*log(n))
-        // int rows = matrix.size(),
-		// 	cols = matrix[0].size(),
-        //     r = 0, c = cols - 1;
-        //     int mid = (r+c)/2;
-		// while(r < rows){
-            
-        //     if(matrix[r][0]<=target && matrix[r][c]>=target){
-        //         int s = 0;
-        //         int e = c;
-        //             while(s<=e){
-        //                 mid = (s+e)/2;
-        //                 if(matrix[r][mid]==target)return true;
-        //                 else if(matrix[r][mid]>target) e=mid-1;
-        //                 else s = mid+1;
-        //             }
-        //     }
-        //     r++;
-        // }
-        // return false;
-      
+        vector<int>arr;
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-      //second approach O(log(m*n));
-      
-      int m = matrix.size();
-	  int n = matrix[0].size();
-      int s=0;
-      int e = m*n-1;
-      int mid = (s+e)/2;
-      while(s<=e){
-          
-          int row = mid / n ;
-          int col = mid % n;
-          if(matrix[row][col]==target)return true;
-          else if(matrix[row][col] > target) e = mid-1;
-          else s = mid+1;
-          mid = (s+e)/2;
-      }
-      return false;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                cout<<matrix[i][j]<<" ";
+                arr.push_back(matrix[i][j]);
+            }
+        }
+       
+        int s =0;
+        int e = arr.size();
+
+        while(s<e){
+            int mid = (s + e )/2;
+            if(arr[mid] == target)return 1;
+            else if(arr[mid] > target)e = mid;
+            else s = mid+1;
+        }
+        if(matrix[0][0] == target)return 1;
+        return 0;
     }
 };
