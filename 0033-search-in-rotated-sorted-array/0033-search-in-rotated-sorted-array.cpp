3f7c67d63c9dad08e7652ef25ce2e630c3225016
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2024-07-06 22:57:17 +0530

    Time: 7 ms (16.54%), Space: 13.5 MB (77.84%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,50 +1,72 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int pidx =0;
 
-        int low = 0;
-        int high = nums.size()-1;
-        if(nums.size() == 1){
-            if(nums[0]==target)return 0;
-            return -1;
-        }
-        while(low < high){
-            int mid = (high + low)/2;
-            if(nums[mid] == nums[0] && nums[mid] == nums[high]){
-                low++;
-                high--;
+        int n = nums.size();
+        int low = 0, high = n-1;
+        while(low <= high){
+            int mid = (low + high) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[low] <= nums[mid]){
+                if(nums[low] <= target && nums[mid] >= target){
+                    high = mid-1;
+                }else{
+                    low = mid+1;
+                }
+           }else{
+            if(nums[high] >= target && nums[mid] <= target){
+                low = mid+1;
+            }else{
+                high = mid-1;
             }
-           else if(nums[mid] >= nums[0]){
-                low = mid +1;
-            }else {
-                high = mid;
-            }
+           }
         }
-        cout<<low;
-      if(target >= nums[0] && target <= nums[low-1]){
-        int s =0;
-         int e = low-1;
+        return -1;
+    //     int pidx =0;
 
-         while(s <= e){
-            int mid = (s + e)/2;
+    //     int low = 0;
+    //     int high = nums.size()-1;
+    //     if(nums.size() == 1){
+    //         if(nums[0]==target)return 0;
+    //         return -1;
+    //     }
+    //     while(low < high){
+    //         int mid = (high + low)/2;
+    //         if(nums[mid] == nums[0] && nums[mid] == nums[high]){
+    //             low++;
+    //             high--;
+    //         }
+    //        else if(nums[mid] >= nums[0]){
+    //             low = mid +1;
+    //         }else {
+    //             high = mid;
+    //         }
+    //     }
+    //     cout<<low;
+    //   if(target >= nums[0] && target <= nums[low-1]){
+    //     int s =0;
+    //      int e = low-1;
 
-            if(nums[mid] == target)return mid;
-            else if(nums[mid] > target)e  = mid-1;
-            else s = mid+1;
-         }
-      }else{
-        int s =low;
-         int e = nums.size()-1;
+    //      while(s <= e){
+    //         int mid = (s + e)/2;
 
-         while(s <= e){
-            int mid = (s + e)/2;
+    //         if(nums[mid] == target)return mid;
+    //         else if(nums[mid] > target)e  = mid-1;
+    //         else s = mid+1;
+    //      }
+    //   }else{
+    //     int s =low;
+    //      int e = nums.size()-1;
 
-            if(nums[mid] == target)return mid;
-            else if(nums[mid] > target)e  = mid-1;
-            else s = mid+1;
-         }
-      }
-      return -1;
+    //      while(s <= e){
+    //         int mid = (s + e)/2;
+
+    //         if(nums[mid] == target)return mid;
+    //         else if(nums[mid] > target)e  = mid-1;
+    //         else s = mid+1;
+    //      }
+    //   }
+    //   return -1;
     }
 };
