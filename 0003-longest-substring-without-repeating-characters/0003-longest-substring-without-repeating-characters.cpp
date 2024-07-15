df1e0451d7245a0adc9e5defd9e0a9ad2cc5c5b8
Tinku Verma <93002064+tinku06071999@users.noreply.github.com>
2024-07-15 09:36:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-       int count =0;
-       int maxi=0;
-       int j=0;
-       unordered_map<char,int>mp;
-      
-       for(int i=0;i<s.size();i++){
-              mp[s[i]]++;
-              maxi++;
-              while(mp[s[i]]>1){
-                  mp[s[j++]]--;
-                  maxi--;
-              }
-              count = max(maxi,count);
-           }
-       return count;
+        unordered_map<char, int>mp;
+        int maxi =0;
+        int count =0;
+        int j =0;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+            count++;
+            while(mp[s[i]] > 1){
+                mp[s[j++]]--;
+                count--;
+            }
+        }
+        return maxi = max(maxi, count);
     }
 };
